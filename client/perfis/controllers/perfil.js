0b447869690c948238f377db6442be8742b91359
Tadashi <tadasshi@gmail.com>
2015-10-04 21:14:43 -0300

    renomeia um controller para loginController2 que estava com o mesmo nome do loginController

--- client/perfis/controllers/perfil.js
+++ client/perfis/controllers/perfil.js
@@ -1,4 +1,4 @@
-angular.module("mentorias").controller("loginController", ['$scope', '$meteor', '$state',
+angular.module("mentorias").controller("loginController2", ['$scope', '$meteor', '$state',
     function ($scope, $meteor, $state) {
         //lógica para visualizar perfil
     }
