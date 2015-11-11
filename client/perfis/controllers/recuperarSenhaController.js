255ddcc8290fc8e54881d37a746f70a178b3656f
Tadashi <tadasshi@gmail.com>
2015-11-11 20:09:43 -0200

    - Commit ligado as issues #26 e #27. Telas e ajustes menores em outras issues.

---
+++ client/perfis/controllers/recuperarSenhaController.js
@@ -0,0 +1,23 @@
+angular.module("mentorias").controller("recuperarSenhaController", ['$scope', '$stateParams', '$meteor', '$state', '$q',
+    function ($scope, $stateParams, $meteor, $state, $q) {
+
+        /*var vm = this;
+
+        vm.credentials = {
+            email: ''
+        };
+
+        vm.error = '';
+
+        vm.reset = function () {
+            $meteor.forgotPassword(vm.credentials.email).then(
+                function () {
+                    $state.go('parties');
+                },
+                function (err) {
+                    vm.error = 'Error sending forgot password email - ' + err;
+                }
+            );
+        };*/
+    }
+]);
