8e1865886969041235fff1ee3f8fc4032404fa85
Jefferson Maran <jefferson.maran@gmail.com>
2015-11-24 19:04:38 -0200

    detalhes empresa com campos editaveis falta salvar com meteor

--- client/empresa/controllers/empresaDetailsController.js
+++ client/empresa/controllers/empresaDetailsController.js
@@ -1,60 +1,57 @@
 if (Meteor.isClient) {
 
 /*Controller para lidar com a página de visualização da empresa. */
-angular.module("mentorias").controller("empresaDetailsController", ["$scope", "$rootScope", "$stateParams", "$meteor",
-   function ($scope, $rootScope, $stateParams, $meteor) { 	    
+angular.module("mentorias").
+controller("empresaDetailsController", ['$scope', '$rootScope', '$stateParams', '$meteor', '$state','$meteorSubscribe',
+    function ($scope, $rootScope, $stateParams, $meteor, $state, $meteorSubscribe) {
 
- 		idEmpresa = $stateParams.empresaId;
-   		$scope.Empresa = $meteor.object(Empresas, idEmpresa);
+    	//bind geral
+		var vm = this;
 
-   		//$scope.Integrantes = $meteor.object(Integrantes);
 
-   		$meteor.subscribe('allUsers').then(function(usersHandle){
-   			allUsers = $meteor.collection(Meteor.users);
-   				arrayUsers = $.map(allUsers, function(value, index) {
-    			return [value];
-			});
-			var usuario =  Meteor.userId();
-			$scope.usuarios = arrayUsers;
-   		})
+   		$meteor.subscribe('empresaByID', $stateParams).then(function(empresaIDHandle){
+   			$scope.ObjEmpresa = $meteor.collection(Empresas, false).subscribe('empresaByID', $stateParams);
+   			console.log($scope.ObjEmpresa); 
 
-   		$meteor.subscribe('empresaByID', $stateParams).then(function(empresaHandle){
-   			empresaByID = $meteor.collection(Empresas);
-   			//console.log(empresaByID);
-   			arrEmpresa = $.map(empresaByID, function(valor, idx){
+   			arrEmpresa = $.map($scope.ObjEmpresa, function(valor, idx){
    				return [valor];
-   			})
-   			$scope.detalhesEmpresa = arrEmpresa[0];
-   			//console.log($scope.detalhesEmpresa);
-
-   			imgid = $scope.detalhesEmpresa.profilePic._id;
-   			
-   			//console.log(imgid);
-
-   			$scope.arr = $meteor.subscribe('imagensID', imgid).then(function(imgHandle){
-	   			arr = $meteor.collectionFS(Images, false, Images);
-	   			img = $.map(arr, function(v, i){
-	   				return [v];
-	   			});
-	   			//$scope.imagemEmpresa = img[0];
    			});
-   		})
-	
-		$meteor.subscribe('produtos').then(function(produtosHandle){
-   			produtos = $meteor.collection(Produtos);
-   			arrProdutos = $.map(produtos, function(val, ind){
-   				return [val];
-   			})
-   			$scope.listaprodutos = arrProdutos; 
+   			$scope.Empresa = arrEmpresa[0];
+   			$scope.imgid = $scope.Empresa.profilePic._id;
+   			
+   		});
+   		$meteor.subscribe('imagensID', $scope.imgid).then(function(imgHandle){
+	   		arr = $meteor.collectionFS(Images, false, Images).subscribe('imagensID', $scope.imgid);
+	   		img = $.map(arr, function(v, i){
+	   			return [v];
+	   		});
+	   		$scope.imagemEmpresa = img;
+   		});		
 
-   			console.log($scope.listaprodutos);
-   		})
-   		
-   		// console.log($scope.listaprodutos, arrProdutos);
-		//Carregando no template
-		
-   }
+   		//updateEmpresa #EmpresaModel
+   		vm.updateEmpresa = function(){
+
+   			alert('atualizando dados empresa');
+   		}
+		//updateIntegrantes #IntegranteModel
+   		vm.updateIntegrantes = function(){
+   			alert('update no integrante');
+   		}
+   		//deleteInntegrantes #IntegranteModel
+   		vm.deleteIntegrate= function(){
+   			alert('deletando Integrante');
+   		}
+   		//setIntegranteRole #IntegranteModel
+   		vm.setRoleIntegrante= function(){
+   			alert('atribuindo cargo integrante');
+   		}
+
+   		//###########################inspector##################################
+   		console.log($scope.imagemEmpresa);
+		console.log($scope.imgid);
+
+	}
+
 ]);
-
 
 }
